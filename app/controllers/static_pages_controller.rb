a5f52743db5de5404fba5e1062180971c11e2c5f
atul <ast0708@gmail.com>
2013-05-31 20:00:41 +0530

    Add a StaticPages controller

---
+++ app/controllers/static_pages_controller.rb
@@ -0,0 +1,9 @@
+class StaticPagesController < ApplicationController
+	#the behavior of this class will be different than 
+	# normal ruby class as it inherits from application controller
+  def home
+  end
+
+  def help
+  end
+end
