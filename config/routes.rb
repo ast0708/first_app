43c98ab5e2ac6ca19c5050ca89cba5a1aea3e576
ast0708 <ast0708@gmail.com>
2013-06-21 17:37:36 +0530

    Finish user signup

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ FirstApp::Application.routes.draw do
 
   root  to: 'static_pages#home'
 
-  match '/signup',  to: 'users#new'
+ match '/signup',  to: 'users#new'
 
   match '/help',     to: 'static_pages#help'
   match '/about',    to: 'static_pages#about'
--- spec/factories.rb
+++ spec/factories.rb
@@ -1,8 +1,8 @@
 FactoryGirl.define do
-	factory :user do
-		name "atul"
-		email "atul@atul.com"
-		password "foobar"
-		password_confirmation "foobar"
-	end
+  factory :user do
+    name     "Michale Hartl"
+    email    "michael@example.com"
+    password "foobar"
+    password_confirmation "foobar"
+  end
 end
